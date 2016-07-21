4ca5b83796be00353c6f8d918ef4a1822b0fd036
Christian Grail <christian.grail@sap.com>
2016-07-21 15:59:33 +0200

    Add Reset when Asteroid leaves the game

---
+++ Assets/Scripts/Resetter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Resetter : MonoBehaviour {
+
+	public Rigidbody2D projectile;
+
+	public float resetSpeed = 0.025f;
+
+	private float resetSpeedSqr;
+	private SpringJoint2D spring;
+
+	// Use this for initialization
+	void Start () {
+		resetSpeedSqr = resetSpeed * resetSpeed;
+		spring = projectile.GetComponent<SpringJoint2D> ();
+	}
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.R)) {
+			Reset ();
+		}
+
+		if (spring == null && projectile.velocity.sqrMagnitude < resetSpeedSqr) {
+			Reset ();
+		}
+	
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.GetComponent<Rigidbody2D>() == projectile) {
+			Reset ();
+		}	
+
+	}
+
+	void Reset () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}
+}
